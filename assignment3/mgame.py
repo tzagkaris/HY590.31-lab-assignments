f8d8f4328dd6cd892a2b3910e1f344b9bc6b9d7f
stzagkarak <stzagkarak@ics.forth.gr>
2023-05-30 13:08:06 +0300

    mgame progress

--- assignment3/mgame.py
+++ assignment3/mgame.py
@@ -17,6 +17,8 @@ import socket
 import threading
 import serial
 import json
+import random
+import copy
 
 # DEFAULT ARGS
 unit_id = 4279;
@@ -24,6 +26,16 @@ HOST = "130.236.81.13";
 PORT = 8716;
 GAME_CONTROL = 0; 
 LOOP_DELAY = 2; 
+
+# OTHER CONFIGURABLE ARGS
+STATE_IDLE = "IDLE"
+IDLE_TIMEOUT = 15
+
+
+# The current state of the game
+gstate = None;
+gunits = None;
+cunit_index = None; # the index of the unit we are waiting for click event.
 
 # block till data arrives
 serial.Serial.timeout = None;
@@ -51,6 +63,9 @@ def parse_argv():
             case _: 
                 raise Exception("Argument Parsing Error");
 
+def GameButtonPress():
+    pass;
+
 # connect to Serial and loop till data arrives
 # if data is **** then call GameButtonPress
 def gatewayLoop():
@@ -58,14 +73,14 @@ def gatewayLoop():
     ser = serial.Serial('/dev/ttyACM0',9600)
 
     while(True):
-        # block here until 4 bytes arrive;
+        # block here until 4 bytes arrive; see line 41
         data = ser.read(4).decode("utf-8")
         # TODO fill this with Vaggos
         
         #if(True):
         #    GameButtonPress()
 
-# start a thread tha will keep watch at arduino Serial
+# start a thread that will keep watch at arduino Serial
 # for button presses and will send messages to host if needed be
 def initGateway():
     threading.Thread(target=gatewayLoop, args=(1, ), daemon=True)
@@ -93,18 +108,98 @@ def SendToHost(message):
 # __ONLY__ WHEN GAME CONTROL IS ACTIVE 
 # get Unit Lists 
 def GameListUnits():
-    return "units";
+
+    list_units_message = {"message_type":"GameListUnits","timeout":60}
+    res = SendToHost(list_units_message);
+    
+    gunits = res["units"];
+    return;
+
+# check if response was successful
+def success_check(response, er_msg):
+    if(response["success"] != "True"):
+        raise Exception(er_msg, response);
+    return;
 
 # __ONLY__ WHEN GAME CONTROL IS ACTIVE 
 # start a new game, 
-# define order and sent appropriate messages.
-# all AMBER  
+# define order and sent appropriate StartControl message.
 def StartNewGame():
-    pass;
+
+    random.shuffle(gunits)
+
+    game_control_message = {"game_state":"START","unit_id":unit_id,"message_type":"GameContol"}
+    res = SendToHost(game_control_message)
+
+    success_check(res, "Game Control-Start Game- Error!")
+
+    gstate = STATE_IDLE;
+    return;
+
+# loop for init_idle. Loops through all units and sends an AMBER message to the 
+# current unit. After 15 secs, will move to the next unit, shutting down the prev one.
+# stop looping and end thread exec when gstate != STATE_IDLE
+def idle_loop():    
+    
+    len_units = len(gunits)
+    unit_index = 0;
+
+    light_up_message = {
+        "led_state":"ON",
+        "led_color":"AMBER",
+        "from":unit_id,
+        "message_type":
+        "GameLEDControl",
+        "to":gunits[unit_index]
+    }
+    
+    light_down_message = {
+        "led_state":"OFF",
+        "led_color":"AMBER",
+        "from":unit_id,
+        "message_type":"GameLEDControl",
+        "to":gunits[unit_index]
+    }
+
+    SendToHost(copy.deepcopy(light_up_message));
+
+    while(gstate == STATE_IDLE):
+        
+        time.sleep(IDLE_TIMEOUT)
+        last_index = unit_index;
+        unit_index = (unit_index + 1)%len_units; 
+        
+        cunit_index = unit_index;
+
+        tldm = copy.deepcopy(light_down_message)
+        tlum = copy.deepcopy(light_up_message)
+
+        tldm["to"] = gunits[last_index];
+        SendToHost(tldm)
+        tlum["to"] = gunits[unit_index];
+        SendToHost(tlum)
+
+    light_down_copy = copy.deepcopy(light_down_message)
+    light_down_copy["to"] = gunits[unit_index];
+    
+    SendToHost(light_down_copy)
+    return;
+
+# start a thread to handle idle state
+def init_idle():
+    threading.Thread(target=idle_loop, args=(1, ))
+    return;
 
 # register the unit_id in the host
 def GameRegister():
-    pass;
+
+    game_register_message = {"unit_id":unit_id,"message_type":"GameRegister","registration_type":"register"}
+    res = SendToHost(game_register_message);
+    
+    success_check(res, "Game Register Error!")
+
+    return;
+
 
 # unregister the unit_id in the host
 def GameUnregister():
@@ -112,20 +207,53 @@ def GameUnregister():
 
 # get a list of tasks for this unit
 def GameGetTasks():
+
+    get_task_message = {"unit_id":unit_id,"message_type":"GameGetTasks"}
+    res = SendToHost(get_task_message)
+
+    return res["tasks"]
+
+# send an n length message to the arduino using serial
+def sendToDevice(string_message):
     pass;
 
-def GameButtonPress():
+def hanldePlayGame():
     pass;
 
-# handle & execute all available tasks
-# return a list of required actions
-def GameHandleTasks(tasks):
+def handleChangeLed():
+    pass;
+
+def handlePlaySound():
     pass;
 
 # __ONLY__ WHEN GAME CONTROL IS ACTIVE 
-# for each action, send appropriate message to host
-def GamePostActions(actions):
+def handleGameButtonPress(actions):
     pass;
+
+# handle available tasks / filter button events if needed.
+# return a list of required actions
+# loop through all tasks
+#   if task is GameLedControl call for your unit id - call handler function
+#   if task is GameControl (start / stop ) - call handler function
+#   if task is GameSoundControl - call handler function
+#   
+#   if task is GameButtonControl - add it to returned actions list
+def GameHandleTasks(tasks):
+
+    actions = []
+
+    for task in tasks:
+        match task["data"]["action"]:
+            # propagate information to arduino using handler functions.
+            case "PLAY_GAME": hanldePlayGame(task);
+            case "CHANGE_LED": handleChangeLed(task);
+            case "PLAY_SOUND": handlePlaySound(task);
+
+            # handle this only when you are game master
+            case "BUTTON_CHANGE": actions.append(task);
+    
+
+    return actions;
 
 
 # ENTRY POINT
@@ -136,24 +264,30 @@ def main():
     try:
         GameRegister();
         if GAME_CONTROL:
-            units = GameListUnits() 
-            StartNewGame(units)
+            GameListUnits() 
+            StartNewGame()
+            init_idle()
     except Exception as err:
+        print("Initialization Error:")
         print(err)
         exit(-1);
     
+    # MAIN LOOP
     while(True):
         try:
             tasks = GameGetTasks()
             actions = GameHandleTasks(tasks)
             if GAME_CONTROL:
-                GamePostActions(actions)
+                handleGameButtonPress(actions)
         
         except Exception as err:
             # what should be done when error happens ? ?
+            print("Game Loop Error:")
             print(err);
 
         time.sleep(LOOP_DELAY);
+
+    #GameUnregister()
                     
 
 if __name__ == '__main__':    
--- assignment3/temp.py
+++ assignment3/temp.py
@@ -7,6 +7,9 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.connect(("130.236.81.13", 8716))
 
 request = {"unit_id":4279,"message_type":"GameRegister","registration_type":"unregister"}
+#request = {"unit_id":4279,"message_type":"GameRegister","registration_type":"register"}
+#request = {"unit_id":4279,"message_type":"GameGetTasks"}
+#request = {"message_type":"GameListUnits","timeout":60}
 print("{}|".format(json.dumps(request)).encode())
 sock.send("{}|".format(json.dumps(request)).encode())    
 receive = sock.recv(1024)
